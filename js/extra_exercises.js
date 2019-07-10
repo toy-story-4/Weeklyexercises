a3d1b36375cd4f0ad6efccbc3aa1db5d75a0f74f
Peter Hollman <peter@Peters-MacBook-Air.local>
2019-07-10 11:40:23 -0500

    bonus exercise

---
+++ js/extra_exercises.js
@@ -0,0 +1,10 @@
+var countLetters = prompt("Enter a string");
+while(countLetters.length === 0){
+    countLetters = prompt("Please enter something")
+}
+alert(countLetters + " has " + countLetters.length + " total characters");
+
+
+
+
+console.log(countLetters);
