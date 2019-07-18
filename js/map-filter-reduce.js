7c3197732e31f216106a432f2e6f671b489d9af8
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-07-18 17:15:00 -0500

    adding bonus exercise for unique languages

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// const filterLanguages = new Set(allTheLanguages);
+// console.log(filterLanguages);
+const uniqueLanguages = users.reduce(function(prev, current){
+    const langs = current.languages;
+    for (const lang of langs){
+        prev.add(lang);
+    }
+    return prev;
+}, new Set);
+console.log(uniqueLanguages);
+//new Set checks for unique values. It does the sorting for you.
